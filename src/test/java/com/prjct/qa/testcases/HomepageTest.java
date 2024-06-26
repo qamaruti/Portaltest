a8cc886480ae3ee1859fc2d940f395c81b90db93
Marutik <mkokane@bastiansolutions.com>
2024-06-26 10:30:03 +0530

    26-06-2024

---
+++ src/test/java/com/prjct/qa/testcases/HomepageTest.java
@@ -0,0 +1,46 @@
+package com.prjct.qa.testcases;
+
+import java.time.Duration;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.prjct.qa.base.TestBase;
+import com.prjct.qa.pages.HomePage;
+import com.prjct.qa.pages.LoginPage;
+
+public class HomepageTest extends TestBase {
+
+	LoginPage loginPage;
+	HomePage homePage;
+
+	public HomepageTest() {
+		super();
+	}
+
+	@BeforeMethod
+	public void setup() throws InterruptedException {
+
+		initilization();
+		loginPage = new LoginPage();
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+
+	}
+
+	@Test
+	public void clickVerifyFavoritesPageTitile() {
+		homePage.clickFavoritiesMenu();
+		String title1 = homePage.VerifyFavoritesPageTitile();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		Assert.assertEquals(title1, "FAVORITES");
+		System.out.print(title1);
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.close();
+	}
+
+}
